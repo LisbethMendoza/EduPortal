c0f36b48333d9f3151ad544139aaa14ea0e81834
LisbethMendoza <lisbethango223019@gmail.com>
2025-08-04 11:34:09 -0400

    feat: visualizacion de avisos bien

--- estudiante/urls.py
+++ estudiante/urls.py
@@ -3,4 +3,10 @@ from . import views
 
 urlpatterns = [
     path('', views.pagina_estudiante, name='inicio_estudiante'),
+    path('registro', views.Link_resgistro, name='registro'),
+    path('Csolicitud', views.Consulta_Solicitud, name='Csolicitud'),
+    path('inscripcion', views.Link_inscripcion, name='inscripcion'),
+    path('reinscripcion', views.Link_reinscripcion, name='reinscripcion'),
+    path('Consulta_avisos', views.Consulta_avisos, name='Consulta_avisos'),
+    
 ]
--- estudiante/views.py
+++ estudiante/views.py
@@ -1,5 +1,25 @@
 
 from django.shortcuts import render
+from avisos.models import Aviso
+from django.http import JsonResponse
+
 
 def pagina_estudiante(request):
     return render(request, 'P_Estudiante.html')
+
+def Link_resgistro(request):
+    return render(request, 'registro.html')
+
+def Consulta_Solicitud(request):
+    return render(request, 'C_solicitud_E.html')
+
+def Link_inscripcion(request):
+    return render(request, 'inscripcion.html')
+
+def Link_reinscripcion(request):
+    return render(request, 'reinscripcion.html')
+
+
+def Consulta_avisos(request):
+    avisos = Aviso.objects.filter(estado="activo").values('titulo', 'descripcion', 'fecha_publi')
+    return JsonResponse(list(avisos), safe=False)
