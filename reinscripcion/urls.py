42bb803d2727d961f044edee4d0bc499f8ae0b1c
LisbethMendoza <lisbethango223019@gmail.com>
2025-08-18 13:15:27 -0400

    feat: Contrato Listo

--- reinscripcion/urls.py
+++ reinscripcion/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path ('documentacion_re', views.reinscripcion_re, name='documentacion_re'),
     path('buscar-estudiante/', views.buscar_estudiante, name='buscar_estudiante'),
     path('reinscripcion_insert/', views.reinscripcion_insert, name='reinscripcion_insert'),
+    path('contrato/<str:codigo>/', views.generar_contrato, name='generar_contrato'),
+
     
 ]
--- reinscripcion/views.py
+++ reinscripcion/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,redirect,get_object_or_404
+from django.shortcuts import render,redirect
 from grado.models import Grado
 from tecnico.models import Tecnico
 from inscripcion.models import Inscripcion
@@ -6,6 +6,78 @@ from estudiante.models import Estudiante
 from django.http import JsonResponse
 from reinscripcion.models import Reinscripcion
 from datetime import date
+from django.http import HttpResponse
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import A4
+from django.utils import timezone
+from django.conf import settings
+import os
+
+
+def generar_contrato(request, codigo):
+    try:
+        estudiante = Estudiante.objects.get(codigo=codigo)
+    except Estudiante.DoesNotExist:
+        return HttpResponse("Estudiante no encontrado", status=404)
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'inline; filename="contrato_{estudiante.nombre}.pdf"'
+
+    c = canvas.Canvas(response, pagesize=A4)
+    width, height = A4
+    
+    ruta_logo = os.path.join(settings.BASE_DIR, 'static', 'imagenes', 'logo.jpg')
+    # Encabezado
+    
+    c.setFont("Helvetica-Bold", 16)
+    c.drawImage(ruta_logo, width / 2 - 50, height - 100, width=100, height=50)
+    c.drawCentredString(width / 2, height - 50, "Contrato de Reinscripción Politécnico Ana Silva Jiménez de Castro")
+
+    # Fecha actual
+    fecha = timezone.now().strftime("%d/%m/%Y")
+    c.setFont("Helvetica", 12)
+    c.drawString(50, height - 100, f"Fecha: {fecha}")
+
+    c.drawString(50, height - 130, f"Nombre del estudiante: {estudiante.nombre} {estudiante.apellido}")
+    c.drawString(50, height - 160, f"Nombre del tutor: {estudiante.tutor.nombre} {estudiante.tutor.apellido}")
+
+    # Texto del contrato
+    texto_contrato = (
+    """Por la presente, me dirijo a usted con el fin de formalizar la reinscripción de mi hijo(a) 
+en el Politécnico Ana Silva Jiménez de Castro para el próximo período académico. Reconozco la 
+importancia de la educación continua y deseo que mi hijo(a) siga recibiendo la formación integral 
+que esta institución ofrece, tanto en el ámbito académico como en el desarrollo de valores y 
+habilidades sociales.
+
+Asimismo, como tutor(a), me comprometo a cumplir estrictamente con las normas y reglamentos 
+establecidos por la institución. Entiendo que la participación activa de mi hijo(a) en todas las 
+actividades académicas y extracurriculares, según el calendario escolar, es fundamental para su 
+desarrollo y aprovechamiento educativo.
+
+Cualquier incumplimiento de estas normas podrá derivar en medidas disciplinarias, las cuales 
+aceptaré y respetaré como parte del compromiso con la formación de mi hijo(a). Confío en que la 
+institución continuará proporcionando un ambiente seguro, respetuoso y propicio para el aprendizaje, 
+contribuyendo al crecimiento integral de mi hijo(a).
+
+Agradezco de antemano la atención prestada a esta reinscripción y quedo a disposición para cumplir 
+con cualquier requisito adicional que sea necesario para completar el proceso formal."""
+)
+
+
+    y = height - 200
+    for linea in texto_contrato.split('\n'):
+        c.drawString(50, y, linea)
+        y -= 20
+
+    # Línea para la firma
+    c.drawString(50, y - 50, " Firma del tutor: ___________________________")
+
+    c.showPage()
+    c.save()
+    return response
+
+
+
 
 def C_Tecnico(request):
     grados = Grado.objects.filter(estado='activo')
@@ -42,6 +114,7 @@ def buscar_estudiante(request):
             siguiente_grado = grado_actual
 
         data = {
+            'codigo': estudiante.codigo, 
             'nombre': f"{estudiante.nombre} {estudiante.apellido}",
             'grado_id': siguiente_grado.id_grado,
             'tecnico_id': estudiante.tecnico.id_tecnico if estudiante.tecnico else None,
